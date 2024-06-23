91fd33bd9941b86962bde29b2824500d45e0672d
YoungjaeKim <kim.youngjae@linecorp.com>
2024-06-23 17:11:52 +0900

    solved BOJ2573

--- src/boj/BOJ2573.kt
+++ src/boj/BOJ2573.kt
@@ -1,70 +1,126 @@
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.util.*
 
 /**
  * Problem : https://www.acmicpc.net/problem/2573
  * TimeComplexity:
- * Algorithim: 브루트포스? DFS, BFS
+ * Algorithim:  DFS, BFS
  * Solution
- * 이중 행렬로 입력을 받고,
- * 이중 행렬을 돌면서 동서남북의 0의 갯수를 세서 1년씩 지나가도록 만들어본다.
- * 빙산이 분리가 되었는지 아닌지는 어떻게 판단할 수 있을까?
+ *
+ * 1년 지날때마다 빙산 녹이기
+ *   실제 배열에 반영하면 계산 결과가 다음 빙산에 영향을 미치기때문에 따로 관리 해야하고
+ *   따로 관리를 n*n 행렬로 만들면 낭비이기때문에 리스트로 만들자
+ *   그리고 0일때마다 바다로 행렬과 리스트에 반영한다.
+ *   녹일때는 리스트를 돌면서 행렬의 4방향을 확인해서 줄인다.
+ *   리스트를 한번더 돌면서 0보다 작으면 이중행렬과 리스트에 반영
+ *
+ * 빙산이 따로 떨어졌는지 확인하기
+ *   DFS로 순회하면서 4
  *
  *
  */
 
+private lateinit var sea: Array<IntArray>
+private lateinit var visited: Array<BooleanArray>
+
+private val nearX = intArrayOf(0, 0, -1, 1)
+private val nearY = intArrayOf(-1, 1, 0, 0)
+
+
 fun main() {
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (n, m) = br.readLine().split(" ").map { it.toInt() }
-    val bingsan = Array(n) {
+    sea = Array(n) {
         br.readLine().split(" ").map { it.toInt() }.toIntArray()
     }
 
-    println("=========")
-    bingsan.forEach { it ->
-        println(it.joinToString("   "))
-    }
-    println("=========")
+    visited = Array(n) { BooleanArray(m) { true } }
+    val iceList: MutableList<Ice> = mutableListOf()
 
-    val nearX = intArrayOf(0, 0, -1, 1)
-    val nearY = intArrayOf(-1, 1, 0, 0)
-
-    fun afterYear() {
-        for (i in 0 until n) {
-            for (j in 0 until m) {
-                val now = bingsan[i][j]
-                if (now == 0)
-                    continue
-
-                var warterCnt = 0
-                for (k in 0 until 4) {
-                    val nx = nearX[k] + i
-                    val ny = nearY[k] + j
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < m && bingsan[nx][ny] == 0) {
-                        warterCnt++
-                    }
-                }
-                var ff = now - warterCnt
-                if (ff < 0) {
-                    ff = 0
-                }
-
-                bingsan[i][j] = ff
+    for (i in 0 until n) {
+        for (j in 0 until m) {
+            val height = sea[i][j]
+            if (height > 0) {
+                iceList.add(Ice(i, j, height))
             }
+
         }
     }
 
-    afterYear()
-    println("=========")
-    bingsan.forEach { it ->
-        println(it.joinToString("   "))
-    }
-    println("=========")
+    var year = 1
+    while (iceList.isNotEmpty()) {
+        for (ice in iceList) {
+            // 인접한 곳에 물이 있는지 확인
+            for (i in 0..3) {
+                val nx = nearX[i] + ice.row
+                val ny = nearY[i] + ice.col
+                if (sea[nx][ny] == 0) {
+                    ice.height--
+                }
+            }
+        }
 
-    afterYear()
-    println("=========")
-    bingsan.forEach { it ->
-        println(it.joinToString("   "))
+
+        val iterator = iceList.iterator()
+        while (iterator.hasNext()) {
+            val ice = iterator.next()
+            if (ice.height <= 0) {
+                sea[ice.row][ice.col] = 0
+
+                iterator.remove()
+            } else {
+                visited[ice.row][ice.col] = false
+            }
+        }
+
+        if (iceList.size > 0) {
+            val dfs = dfs(iceList[0].row, iceList[0].col)
+            if (dfs != iceList.size) {
+                println(year)
+                return
+            }
+        }
+
+        year++
     }
-    println("=========")
+    println(0)
 }
+
+private fun dfs(row: Int, col: Int): Int {
+    visited[row][col] = true
+    var cnt = 1
+
+    for (i in 0..3) {
+        val nx = nearX[i] + row
+        val ny = nearY[i] + col
+
+        if (visited[nx][ny]) continue
+        cnt += dfs(nx, ny)
+    }
+    return cnt
+}
+
+private fun bfs(row: Int, col: Int): Int {
+    val queue = LinkedList<Ice>()
+    queue.offer(Ice(row, col, 0))
+    visited[row][col] = true
+
+    var cnt = 0
+    while (queue.isNotEmpty()) {
+        val ice = queue.poll()
+        cnt++
+
+        for (i in 0..3) {
+            val nx = nearX[i] + ice.row
+            val ny = nearY[i] + ice.col
+
+            if (visited[nx][ny]) continue
+            queue.offer(Ice(nx, ny, 0))
+            visited[nx][ny] = true
+        }
+    }
+    return cnt
+}
+
+data class Ice(val row: Int, val col: Int, var height: Int)
